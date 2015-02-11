750c403e5ed1fe35ecf1cf47e4d9c02d03c26a14
chencheng <839464430@qq.com>
2015-02-11 17:02:57 +0800

    no message

---
+++ TestQuartz_截屏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestQuartz_截屏
+//
+//  Created by admin on 15-1-14.
+//  Copyright (c) 2015年 ___CC___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestQuartz_截屏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestQuartz_截屏
+//
+//  Created by admin on 15-1-14.
+//  Copyright (c) 2015年 ___CC___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
